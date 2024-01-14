afc81fcf33983560555b068f85218bb6be7b22a2
PlatonKynin <kininplaton@gmail.com>
2024-01-14 17:19:00 +0300

    Add idea to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./Rating/Rating";
 
+/*function sum(a: any, b: number) {
+
+    alert(a + b)
+}
+sum(23, 12)
+sum(100, 300)*/
+
+
+// function declaration
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // полезное что-то
+    // обязана вернуть JSX
+    console.log("App rendering")
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={"Accordion title 1 !!!"}/>
+            <Accordion title={"Accordion title 2 !!!"}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+function PageTitle(props: any) {
+    console.log("PageTitle rendering")
+    return <h1>{ props.title }</h1>
 }
 
 export default App;
