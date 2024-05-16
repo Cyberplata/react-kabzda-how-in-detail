6012bae2185bf8c5d2ce2ed1c0fae5f599a05d84
PlatonKynin <kininplaton@gmail.com>
2024-05-16 22:09:37 +0300

    08 - callbacks 1 -> зарефакторили передача данных-колбеков value, setValue через пропсы из UncontrolledRating в Star

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -17,26 +17,30 @@ export function UncontrolledRating(props: RatingPropsType) {
 
     return (
         <div>
-            <Star selected={value > 0}
-                  onClickSpanHandler={() => handleStarClick(1)}/>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
 
-            <Star selected={value > 1}
-                  onClickSpanHandler={() => handleStarClick(2)}/>
-
-            <Star selected={value > 2}
-                  onClickSpanHandler={() => handleStarClick(3)}/>
-
-            <Star selected={value > 3}
-                  onClickSpanHandler={() => handleStarClick(4)}/>
-
-            <Star selected={value > 4}
-                  onClickSpanHandler={() => handleStarClick(5)}/>
+            {/*<Star selected={value > 0}*/}
+            {/*      onClickSpanHandler={() => handleStarClick(1)}/>*/}
+            {/*<Star selected={value > 1}*/}
+            {/*      onClickSpanHandler={() => handleStarClick(2)}/>*/}
+            {/*<Star selected={value > 2}*/}
+            {/*      onClickSpanHandler={() => handleStarClick(3)}/>*/}
+            {/*<Star selected={value > 3}*/}
+            {/*      onClickSpanHandler={() => handleStarClick(4)}/>*/}
+            {/*<Star selected={value > 4}*/}
+            {/*      onClickSpanHandler={() => handleStarClick(5)}/>*/}
         </div>
     );
 }
 
 type StarPropsType = {
     selected: boolean
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
+    value: 1 | 2 | 3 | 4 | 5
     // onClickSpanHandler: () => void
 }
 
@@ -47,8 +51,8 @@ function Star(props: StarPropsType) {
     //     ? <span onClick={props.onClickSpanHandler}><b>star</b> </span>
     //     : <span onClick={props.onClickSpanHandler}>star </span>
 
-    return <span>
-        { props.selected ? <b>star</b> : "star" }
+    return <span onClick={() => {props.setValue(props.value)}}>
+        { props.selected ? <b>star </b> : "star " }
     </span>
 
     // if (props.selected === true) {
