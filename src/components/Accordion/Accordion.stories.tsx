9312c84f37d5b73ebd7ca2e32a7aba58f65e6d12
PlatonKynin <kininplaton@gmail.com>
2024-05-27 22:11:24 +0300

    10-1. Storybook введение->Сделали Actions с помощью легаси import -> action

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,4 +1,5 @@
 import type {Meta, StoryObj} from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import Accordion from './Accordion';
 import React, {useState} from "react";
@@ -9,12 +10,22 @@ const meta: Meta<typeof Accordion> = {
 
 export default meta;
 
+type Story = StoryObj<typeof Accordion>;
+
+export const FirstStory: Story = {
+    args: {
+        titleValue: "hello",
+        collapsed: true,
+        onChange: () => {},
+    },
+};
+
+const onChangeHandler = action('onChange')
+
 export const CollapsedAccordion = () => {
     return <Accordion titleValue={"Collapsed Accordion"}
                       collapsed={true}
-                      onChange={() => {
-                          console.log('on change')
-                      }}/>
+                      onChange={onChangeHandler}/>
 }
 
 export const OpenedAccordion = () => {
