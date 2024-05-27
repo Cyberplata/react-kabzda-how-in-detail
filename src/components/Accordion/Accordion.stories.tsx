48d4a16942c78c61e1d8fe120b2ac208c6bda85c
PlatonKynin <kininplaton@gmail.com>
2024-05-27 21:27:08 +0300

    10-1. Storybook введение->Добавили 3 состояния компоненты Accordion.stories.tsx

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,19 +1,45 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type {Meta, StoryObj} from '@storybook/react';
 
 import Accordion from './Accordion';
+import React, {useState} from "react";
 
 const meta: Meta<typeof Accordion> = {
     component: Accordion,
 };
 
 export default meta;
-type Story = StoryObj<typeof Accordion>;
 
-export const Default: Story = {
-    args: {
-        dataType: 'latency',
-        showHistogramLabels: true,
-        histogramAccentColor: '#1EA7FD',
-        label: 'Latency distribution',
-    },
-};
+export const CollapsedAccordion = () => {
+    return <Accordion titleValue={"Collapsed Accordion"}
+                      collapsed={true}
+                      onChange={() => {}}
+    />
+}
+
+export const OpenedAccordion = () => {
+    return <Accordion titleValue={"Opened Accordion"}
+                      collapsed={false}
+                      onChange={() => {}}
+    />
+}
+
+export const AccordionDemo = () => {
+    const [collapsed, setCollapsed] = useState(false)
+    return <Accordion titleValue={"Accordion"}
+                      collapsed={collapsed}
+                      onChange={() => {
+                          setCollapsed(!collapsed)
+                      }}
+    />
+}
+
+// type Story = StoryObj<typeof Accordion>;
+//
+// export const Default: Story = {
+//     args: {
+//         dataType: 'latency',
+//         showHistogramLabels: true,
+//         histogramAccentColor: '#1EA7FD',
+//         label: 'Latency distribution',
+//     },
+// };
