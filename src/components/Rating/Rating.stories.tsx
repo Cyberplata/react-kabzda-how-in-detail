e865685e53a984bd37e99d0b65edd8dfa60e13bd
PlatonKynin <kininplaton@gmail.com>
2024-06-02 17:44:42 +0300

    10-2.Storybook->Добавил Rating1 | Rating2 | Rating3 etc in Rating.stories.tsx

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -4,24 +4,50 @@ import {Rating, RatingValueType} from "./Rating";
 import React, {useState} from "react";
 
 export default {
+    // title: "Rating stories",
     component: Rating,
 };
 
 const onChangeHandler = action('onClick')
 
-export const RatingEmpty = () => {
+export const EmptyRating = () => {
     return <Rating value={0}
                    onClick={onChangeHandler}
     />
 }
 
-export const RatingFilled = () => {
+
+export const Rating1 = () => {
+    return <Rating value={1}
+                   onClick={onChangeHandler}
+    />
+}
+
+export const Rating2 = () => {
+    return <Rating value={2}
+                   onClick={onChangeHandler}
+    />
+}
+
+export const Rating3 = () => {
+    return <Rating value={3}
+                   onClick={onChangeHandler}
+    />
+}
+
+export const Rating4 = () => {
+    return <Rating value={4}
+                   onClick={onChangeHandler}
+    />
+}
+
+export const FilledRating = () => {
     return <Rating value={5}
                    onClick={() => {}}
     />
 }
 
-export const RatingDynamic = () => {
+export const DynamicRating = () => {
     const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
     return <Rating value={ratingValue}
                    onClick={setRatingValue}
