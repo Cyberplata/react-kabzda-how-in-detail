7aafd7800ddadbb16e241434be0b1153252483e4
PlatonKynin <kininplaton@gmail.com>
2024-07-02 21:36:23 +0300

    17 - React.memo => Иммутабельно добавляем в стейт users и всё у нас перерисовывается

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -18,12 +18,17 @@ const UsersSecret = (props: { users: Array<string> }) => {
 const Users = React.memo(UsersSecret)
 
 export const Example1 = () => {
+    console.log("Example1")
+
     const [counter, setCounter] = useState(0);
     const [users, setUsers] = useState(["Dimych", "Valera", "Artem", "Katya"]);
 
     const addUser = () => {
-        users.push("Sveta" + new Date().getTime())
-        setUsers(users)
+        // users.push("Sveta" + new Date().getTime())
+        setUsers([
+            ...users,
+            "Sveta" + new Date().getTime()
+        ])
     }
 
     return <>
