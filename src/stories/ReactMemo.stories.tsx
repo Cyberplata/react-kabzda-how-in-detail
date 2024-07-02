e51637f2559692f182739adee1e58f06def5290b
PlatonKynin <kininplaton@gmail.com>
2024-07-02 21:28:35 +0300

    17 - React.memo => Создали React.memo - HOC, то есть теперь у нас идёт обособленна компонента Users и при клике на кнопку Users не перерисовывается

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useState} from "react";
 
 export default {
     title: "React.memo demo",
@@ -8,12 +8,14 @@ const NewMessageCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
 
-const Users = (props: { users: Array<string> }) => {
+const UsersSecret = (props: { users: Array<string> }) => {
     console.log("USERS")
     return <div>
         {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
+
+const Users = React.memo(UsersSecret)
 
 export const Example1 = () => {
     const [counter, setCounter] = useState(0);
