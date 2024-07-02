0ff15b76f45d1bf322be14fdc9095f475291f60a
PlatonKynin <kininplaton@gmail.com>
2024-07-02 23:01:25 +0300

    17 - React.memo =>HW=> Rating.tsx обернул в React.memo

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -8,14 +8,14 @@ type RatingPropsType = {
 }
 
 export function Rating(props: RatingPropsType) {
-    console.log("UncontrolledRating rendering")
+    console.log("СontrolledRating rendering")
     return (
         <div>
-            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
-            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
-            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
-            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
-            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
+            <MemoizedStar selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <MemoizedStar selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <MemoizedStar selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <MemoizedStar selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <MemoizedStar selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     );
 }
@@ -33,3 +33,5 @@ function Star(props: StarPropsType) {
         {props.selected ? <b>star </b> : "star "}
     </span>
 }
+
+const MemoizedStar = React.memo(Star);
