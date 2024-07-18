357f96d8f91e58646d2a134b4845f064377b4595
PlatonKynin <kininplaton@gmail.com>
2024-07-18 23:45:46 +0300

    18-useMemo => Пример для использования useMemo внутри компонента HelpsToReactMemo и убираем тем самым лишние перерисовки

--- src/stories/useMemo.stories.tsx
+++ src/stories/useMemo.stories.tsx
@@ -4,7 +4,7 @@ export default {
     title: 'useMemo',
 }
 
-export const UseMemoStories = () => {
+export const DifficultCountingExample = () => {
     const [a, setA] = useState<number>(5)
     const [b, setB] = useState<number>(5)
 
@@ -12,16 +12,17 @@ export const UseMemoStories = () => {
     let resultB = 1;
 
     resultA = useMemo(() => {
+        // debugger
         let tempResultA = 1;
         for (let i = 1; i <= a; i++) {
-            let fake = 0
+            let fake = 0;
             while (fake < 100000000) {
                 fake++;
                 const fakeValue = Math.random();
             }
             tempResultA = tempResultA * i;
         }
-        return tempResultA
+        return tempResultA;
     }, [a])
 
 
@@ -41,3 +42,40 @@ export const UseMemoStories = () => {
         </div>
     </>
 };
+
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    // debugger
+    console.log("USERS SECRET")
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+    console.log("HelpsToReactMemo")
+
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(["Dimych", "Valera", "Artem", "Katya"]);
+
+    const newMainArray = useMemo(() => {
+        const newArray = users.filter(u => u.toLowerCase().indexOf("a") > -1)
+        return newArray
+    }, [users])
+
+    const addUser = () => {
+        setUsers([
+            ...users,
+            "Sveta " + new Date().getTime()
+        ])
+    }
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => addUser()}>addUser</button>
+        {counter}
+        <Users users={newMainArray}/>
+    </>
+}
