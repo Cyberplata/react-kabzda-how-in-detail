6c6a4a88e5347f359feb3cfeb4d2f8ddbf0debc1
PlatonKynin <kininplaton@gmail.com>
2024-09-08 17:55:01 +0300

    22-useEffect\setTimeout\setInterval -> 03-setInterval. SetWatchEffectExample1 часы с анониманой функцией в useEffect return () => clearInterval(intervalId);

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -46,10 +46,10 @@ export const SetTimeoutExample = () => {
     useEffect(() => {
         console.log("useEffect first render and every counter change")
 
-        setInterval(() => {
-            console.log("tick: " + counter)
-            setCounter(state => state + 1)
-        }, 1000)
+        // setInterval(() => {
+        //     console.log("tick: " + counter)
+        //     setCounter(state => state + 1)
+        // }, 1000)
 
     }, []);
 
@@ -62,4 +62,91 @@ export const SetTimeoutExample = () => {
     </>
 }
 
+export const SetWatchEffectExample = () => {
+    console.log("SetWatchEffectExample")
+
+    // Функция для получения текущего времени в формате HH:MM:SS
+    const getCurrentTime = () => {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const second = String(now.getSeconds()).padStart(2, '0');
+        return `${hours}:${minutes}:${second}`;
+    };
+
+    const [watch, setWatch] = useState(getCurrentTime);
+
+    useEffect(() => {
+        console.log("useEffect first render and every counter change")
+
+        setInterval(() => {
+            console.log("tick: " + watch)
+
+            setWatch((state) => state)
+        }, 1000)
+
+    }, []);
+
+    return <>
+        Hello,
+        <div>Watch: {watch},</div>
+        {/*<div>fake: {fake}</div>*/}
+        {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+        {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+    </>
+}
+
+
+export const SetWatchEffectExample1 = () => {
+    console.log("SetWatchEffectExample1");
+    // debugger
+    // // 1.Функция для получения текущего времени в формате HH:MM:SS
+    // const getCurrentTime = () => {
+    //     const now = new Date();
+    //     return now.toLocaleTimeString();
+    // };
+
+    // // 2. Функция для получения текущего времени в формате HH:MM:SS
+    // const getCurrentTime = () => {
+    //     const now = new Date();
+    //     const hours = String(now.getHours()).padStart(2, '0');
+    //     const minutes = String(now.getMinutes()).padStart(2, '0');
+    //     const second = String(now.getSeconds()).padStart(2, '0');
+    //     return `${hours}:${minutes}:${second}`;
+    // };
+
+    // 3. Функция для получения текущего времени в формате HH:MM:SS
+    const getCurrentTime = () => {
+        const now = new Date();
+        return new Intl.DateTimeFormat('default', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        }).format(now);
+    };
+
+    const [watch, setWatch] = useState(getCurrentTime());
+
+    useEffect(() => {
+        console.log("useEffect first render and every second");
+
+        const intervalId = setInterval(() => {
+            console.log("tick: " + getCurrentTime());
+            setWatch(getCurrentTime());
+        }, 1000);
+
+        // Очистка интервала при размонтировании компонента
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <>
+            Hello,
+            <div>
+                <time dateTime={new Date().toISOString()}>{watch}</time>
+            </div>
+        </>
+    );
+};
 
