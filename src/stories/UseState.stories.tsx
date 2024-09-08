686d686caf41adfc96f2d2e017217df39c0b6f63
PlatonKynin <kininplaton@gmail.com>
2024-09-08 09:23:33 +0300

    20-навороченный useState -> generateData каждый раз вызывается, а это плохо.

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,19 +1,21 @@
-import React, {useState} from "react";  // 0
+import React, {useState} from "react";  // 3281231231231
 
 export default {
     title: "useState demo",
 };
 
 function generateData() {
+    // difficult counting
+    console.log("generateData")
     return 3281231231231;
 }
 
 export const Example1 = () => {
     console.log("Example1")
 
-    generateData()
+    const initValue = generateData()
 
-    const [counter, setCounter] = useState(0); // [0, function(newValue){}]
+    const [counter, setCounter] = useState(initValue); // [3281231231231, function(newValue){}]
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
