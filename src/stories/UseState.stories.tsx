a792111aba077a22030801f2887ea4775bf7f11c
PlatonKynin <kininplaton@gmail.com>
2024-09-08 09:33:26 +0300

    20-навороченный useState -> initialState v3 с комментарием

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -16,6 +16,9 @@ export const Example1 = () => {
     // const initValue = useMemo(generateData, [])
 
     const [counter, setCounter] = useState(generateData); // [3281231231231, function(newValue){}]
+    // 1.
+    // Используем в качестве initialState в useState функцию,
+    // чтобы какой-то тяжелый подсчёт-синхронный не блокировал основной поток и не вызывал его лишний раз
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
